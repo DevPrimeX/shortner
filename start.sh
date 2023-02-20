4afa221e34ede574104a86c257aab98c6d3a489a
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2023-02-20 18:58:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/Proton-2 /Proton-2
+  git clone https://github.com/Lucifer86790/shortner /shortner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Proton-2
+  git clone $UPSTREAM_REPO /shortner
 fi
-cd /Proton-2
+cd /shortner
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
