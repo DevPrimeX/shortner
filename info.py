1b692f2c99b527e83ec54c78c99ef54339216081
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2023-02-28 22:41:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "True")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ojFdI-D20yU4ZDVl')
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
